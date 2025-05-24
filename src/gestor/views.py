23d08a1fae0c2ec45b8373562cb3aa97bdaa3cb9
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-24 16:48:52 -0500

    | views.py | : Se crea vista agregar_curso para manejar formulario de cursos

--- src/gestor/views.py
+++ src/gestor/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from .forms import CursoForm
+from .models import Curso
+
+def agregar_curso(request):
+    if request.method == "POST":
+        form = CursoForm(request.POST)
+        if form.is_valid:
+            form.save()
+            return redirect('lista_cursos')
+    else:
+        form = CursoForm()
+    return render(request, "agregar_curso.html", {"form": form})
