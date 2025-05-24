c6231d5e173ac9da98fbcd1fcfb89995d52d9581
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-24 18:17:34 -0500

    | models.py | : Se agrega nuevo campo en tabla Curso y se aplica migración

--- src/gestor/models.py
+++ src/gestor/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 class Curso(models.Model):
     nombre = models.CharField(max_length=50, unique=True)
+    codigo = models.CharField(max_length=10, unique=True)
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre} ({self.codigo})"
