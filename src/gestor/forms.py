40e2ca57076b247ad6bc6c922a2ba429d9f2bbdf
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-24 18:34:05 -0500

    | forms.py | : Se modifico para que ahora también se pida codigo

--- src/gestor/forms.py
+++ src/gestor/forms.py
@@ -4,4 +4,16 @@ from .models import Curso
 class CursoForm(forms.ModelForm):
     class Meta:
         model = Curso
-        fields = ["nombre"]
+        fields = ["nombre", "codigo"]
+
+    def clean_nombre(self):
+        nombre = self.cleaned_data.get("nombre")
+        if Curso.objects.filter(nombre=nombre).exists():
+            raise forms.ValidationError("Ya existe un curso con este nombre.")
+        return nombre
+
+    def clean_codigo(self):
+        codigo = self.cleaned_data.get("codigo")
+        if Curso.objects.filter(codigo=codigo).exists():
+            raise forms.ValidationError("Código ya registrado")
+        return codigo
