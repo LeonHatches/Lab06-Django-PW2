aa12a99590535af5ada9cee3dacc46071d5ff133
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-24 18:36:29 -0500

    | urls.py | : Se agrega url de lista_cursos

--- src/gestor/urls.py
+++ src/gestor/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("agregar_curso", views.agregar_curso, name="agregar_curso")
+    path("agregar_curso", views.agregar_curso, name="agregar_curso"),
+    path('lista_cursos', views.lista_cursos, name="lista_cursos")
 ]
