ca18d3bdcb9671951f064c0f7797420ed68cb17f
leonhatches <jhatches@unsa.edu.pe>
2025-05-24 19:53:30 -0500

    | views.py | Alumnos | : Se crea una vista para manejar el formulario de crear alumno en views.py

--- src/gestor/views.py
+++ src/gestor/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import AlumnoForm
+from .models import Alumno
 
-# Create your views here.
+def crear_alumno(request):
+    if request.method == 'POST':
+        form = AlumnoForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('lista_alumno')
+    else:
+        form = AlumnoForm()
+    
+    return render(request, 'gestor/crear_alumno.html', {'form':form})
+        
