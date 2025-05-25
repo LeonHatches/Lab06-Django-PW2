63cbca456a305772150efda77b7335ddec6ae6f7
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-24 20:43:40 -0500

    | forms.py | : Se crea form para subir notas

--- src/gestor/forms.py
+++ src/gestor/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Curso
+from .models import Curso, NotaAlumnoPorCurso
 
 class CursoForm(forms.ModelForm):
     class Meta:
@@ -17,3 +17,18 @@ class CursoForm(forms.ModelForm):
         if Curso.objects.filter(codigo=codigo).exists():
             raise forms.ValidationError("Código ya registrado")
         return codigo
+
+class NotaForm(forms.ModelForm):
+    cui_alumno = forms.CharField(max_length=8, label="CUI")
+
+    class Meta:
+        model =  NotaAlumnoPorCurso
+        # fields = ["cui_alumno", "curso", "nota"]
+        fields = ["curso", "nota"]
+
+    # def clean_cui_alumno(self):
+    #     cui = self.cleaned_data.get('cui_alumno')
+    #     if not Curso.objects.filter(cui=cui).exists()
+    #         raise forms.ValidationError("El alumno con este CUI no existe")
+        
+    #     return cui
