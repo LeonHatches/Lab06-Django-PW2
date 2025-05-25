283331392ca751dd01b66cc1823e658768ed41c5
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-24 20:12:46 -0500

    | models.py | : Se definió clase NotaAlumnoPorCurso y se aplico migración

--- src/gestor/models.py
+++ src/gestor/models.py
@@ -6,3 +6,8 @@ class Curso(models.Model):
 
     def __str__(self):
         return f"{self.nombre} ({self.codigo})"
+    
+class NotaAlumnoPorCurso(models.Model):
+    # alumno = models.ForeignKey(Alumno, on_delete=models.CASCADE)
+    curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+    nota = models.DecimalField(max_digits=2, decimal_places=0)
