6480fce348b5e7cdde556dd861d3f53cbefccf69
leonhatches <jhatches@unsa.edu.pe>
2025-05-24 19:20:02 -0500

    | models.py | Alumnos | : Se crea la clase Alumno con CUI, nombres y apellidos

--- src/gestor/models.py
+++ src/gestor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Alumno(models.Model):
+    CUI       = models.CharField(max_length=8, unique=True)
+    nombres   = models.CharField(max_length=200)
+    apellidos = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.CUI
