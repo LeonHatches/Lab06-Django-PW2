f8931e7c824fde5bf65865d4887af876762f31b5
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-24 23:11:04 -0500

    | urls.py | : Se configuran las urls para notas

--- src/gestor/urls.py
+++ src/gestor/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('lista_cursos', views.lista_cursos, name="lista_cursos"),
     path('crear/', views.crear_alumno, name='crear_alumno'),
     path('lista/', views.lista_alumnos, name='lista_alumnos'),
+    path('subir_notas', views.subir_notas, name='subir_notas'),
+    path('lista_notas', views.lista_notas, name='lista_notas'),
 ]
