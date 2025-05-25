1446e44d3fc46c871e2a9542e07d53352a8cc58d
JoaquinQFox <jquispebed@unsa.edu.pe>
2025-05-24 23:27:02 -0500

    | urls.py | : Se modifico nombre de rutas

--- src/gestor/urls.py
+++ src/gestor/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('subir_notas', views.subir_notas, name='subir_notas'),
-    path('lista_notas', views.lista_notas, name='lista_notas'),
+    path('subirNotas/', views.subir_notas, name='subir_notas'),
+    path('listaNotas/', views.lista_notas, name='lista_notas'),
     path('crearCurso/', views.agregar_curso, name="agregar_curso"),
     path('listaCursos/', views.lista_cursos, name="lista_cursos"),
     path('crearAlumno/', views.crear_alumno, name='crear_alumno'),
