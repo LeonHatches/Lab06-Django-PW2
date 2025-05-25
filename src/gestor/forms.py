962dbce7453b4f99bfdac17f6d5b0d2d3337be8b
leonhatches <jhatches@unsa.edu.pe>
2025-05-24 19:43:22 -0500

    | forms.py | Alumnos | : Se crea un mensaje personalizado al colocar un CUI del mismo número

--- src/gestor/forms.py
+++ src/gestor/forms.py
@@ -5,3 +5,8 @@ class AlumnoForm(forms.ModelForm):
     class Meta:
         model = Alumno
         fields = ['CUI', 'nombres', 'apellidos']
+    
+    def clean_CUI(self):
+        CUI = self.cleaned_data.get('CUI')
+        if Alumno.objects.filter(CUI=CUI).exists():
+            raise forms.ValidationError("El CUI ingresado ya EXISTE.")
