1e795c61f521cb1a97ecec3851adcc91033111b3
leonhatches <jhatches@unsa.edu.pe>
2025-05-24 23:07:48 -0500

    | URLs | : Se modifican las URLs de los Cursos y Alumnos del proyecto y la App

--- src/gestion_estudiantes/urls.py
+++ src/gestion_estudiantes/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('gestor/', include('gestor.urls')),
-    path('alumnos/', include('gestor.urls')),
+    path('gestor/', include('gestor.urls')),
 ]
--- src/gestor/urls.py
+++ src/gestor/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("agregar_curso", views.agregar_curso, name="agregar_curso"),
-    path('lista_cursos', views.lista_cursos, name="lista_cursos"),
-    path('crear/', views.crear_alumno, name='crear_alumno'),
-    path('lista/', views.lista_alumnos, name='lista_alumnos'),
+    path("crearCurso/", views.agregar_curso, name="agregar_curso"),
+    path('listaCursos/', views.lista_cursos, name="lista_cursos"),
+    path('crearAlumno/', views.crear_alumno, name='crear_alumno'),
+    path('listaAlumnos/', views.lista_alumnos, name='lista_alumnos'),
 ]
